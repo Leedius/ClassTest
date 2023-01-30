0ebed4bd98c910b235b56aaa0856a355974b66c7
Leedius <tiuy2750@gmail.com>
2023-01-30 09:06:26 +0900

    update

--- src/test7_10/StudentTest.java
+++ src/test7_10/StudentTest.java
@@ -2,14 +2,12 @@ package test7_10;
 
 public class StudentTest {
     public static void main(String[] args) {
-        //Student s1 = new Student("");
+        Student s1 = new Student("홍길동",
+                20,
+                "울산",
+                "01",
+                "010-1234-5678");
 
-//        s1.Student("홍길동",
-//                20,
-//                "울산",
-//                "01",
-//                "010-1234-5678");
-
-        //s1.printStudentInfo();
+        s1.printStudentInfo();
     }
 }
